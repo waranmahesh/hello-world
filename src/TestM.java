ea0947889ae1556eb1232763752b5673c628d85b
Maheshwaran Chinnasamy <waran.mahesh@gmail.com>
2016-12-29 14:35:05 +0100

    Create TestM.java

---
+++ src/TestM.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TestM {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+    SimpleDateFormat sdfrr = new SimpleDateFormat("HH:mm:ss");
+		Date ddatat = sdfrr.parse("00:45:22");
+		System.out.println("Dare:" + ddatat.getTime());
+
+	}
+}
