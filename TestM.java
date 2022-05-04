4bd62052c465009bee06960550fb24afb6254e17
Maheshwaran Chinnasamy <maheshwaran.c@tcs.com>
2022-05-04 10:12:36 +0530

    Added few

--- TestM.java
+++ TestM.java
@@ -12,6 +12,7 @@ public class TestM {
 		SimpleDateFormat sdfrr = new SimpleDateFormat("HH:mm:ss");
 		Date ddatat = sdfrr.parse("00:45:22");
 		System.out.println("Dare:" + ddatat.getTime());
+		System.out.println("Mahesh");
 
 		}
 }
